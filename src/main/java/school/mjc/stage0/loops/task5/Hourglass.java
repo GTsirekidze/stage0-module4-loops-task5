1dae6ed4e3ce4191f9503280ddd4fabf092c90c2
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-21 16:04:55 +0400

    "commit"

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int l=height;
+        for (int i =0;i<height;i++){
 
+            for (int j=0;j<height;j++){
+                if(j>=(height-l)/2 && j<l+(height-l)/2) System.out.print('8');
+                else System.out.print(' ');
+            }
+            if(i<height/2)l-=2;
+            else l+=2;
+            System.out.println();
+        }
     }
 }
